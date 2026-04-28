76e6e5377253d0814e7c651a390dd39486805aa1
DiegoQuito21 <Diegoquito2002@gmail.com>
2026-04-28 17:40:34 -0400

    Add protocol filter flag using argparse

--- analyzer/capture.py
+++ analyzer/capture.py
@@ -2,15 +2,14 @@ from scapy.all import sniff
 from .parser import parse_packet 
 from .display import display_packet , display_summary
 
-def start_capture(packet_count = 1000):
+def start_capture(packet_count = 1000, filter_protocol=None):
     def handle_packet(packet):
         parsed = parse_packet(packet)
-        display_packet(parsed)
-    
+        if filter_protocol is None or parsed["protocol"] == filter_protocol:
+            display_packet(parsed)
     print(f"packet capture starting")
     sniff(count = packet_count , store = False , prn = handle_packet)
     display_summary()
    
 
 
- 
--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import argparse
 from analyzer import start_capture
 
+parser = argparse.ArgumentParser(description="Network Traffic Analyzer")
+parser.add_argument("--filter", type = str, help= "Filter by protocol TCP, UDP , ICMP")
+args = parser.parse_args()
+
 if __name__ =="__main__":
-    start_capture()
+    start_capture(filter_protocol = args.filter)
+    
+
 
     
